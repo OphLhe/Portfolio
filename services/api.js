ee92613301361f864309af3f94823af038cb0c81
Ophelie <ophelie.lhermitte@gmail.com>
2026-03-02 16:34:09 +0100

    Api url

--- services/api.js
+++ services/api.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
+console.log("API URL:", API_URL);
+
 if (!API_URL) {
   throw new Error("VITE_API_URL is not defined in production");
 }
