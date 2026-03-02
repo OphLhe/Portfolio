bc46bdaf3780dcee6dcb9d04ffaed2eb1d1c6255
Ophelie <ophelie.lhermitte@gmail.com>
2026-03-02 17:59:04 +0100

    mailgun

--- components/ContactPage.jsx
+++ components/ContactPage.jsx
@@ -198,7 +198,7 @@ const ContactPage = () => {
                         disabled={isSubmitting}
                       />
                       {isSubmitSuccessful && (
-                        <div className="mb-4 rounded bg-green-100 p-3 text-center text-base text-green-800">
+                        <div className="mt-4 rounded bg-green-100 p-3 text-center text-base text-green-800">
                           Message envoyé avec succès !
                         </div>
                       )}
