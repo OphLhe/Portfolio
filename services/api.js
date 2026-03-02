3ea3cf055aa290509ac3d8ab14b4c0bc009d04f5
Ophelie <ophelie.lhermitte@gmail.com>
2026-03-02 16:31:09 +0100

    Api url

--- services/api.js
+++ services/api.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const API_URL = import.meta.env.VITE_API_URL;
+
+if (!API_URL) {
+  throw new Error("VITE_API_URL is not defined in production");
+}
 
 const API = axios.create({
     baseURL: `${API_URL}/api`,
